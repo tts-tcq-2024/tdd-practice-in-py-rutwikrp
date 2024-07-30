f0abf1dcb1705ba779e49e8e84e0764ec920d80d
rutwikrp <93390786+rutwikrp@users.noreply.github.com>
2024-07-30 12:18:14 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -1,30 +1,36 @@
 import unittest
-from StringCalculator import add
+from string_calculator import StringCalculator
+
 class TestStringCalculator(unittest.TestCase):
-        
-        def test_expectZeroForEmptyInput(self):
-                self.assertEqual(add(""), 0)
-                
-        def test_expectZeroForSingleZero(self):
-                self.assertEqual(add("0"), 0)
-                
-        def test_expectSumForTwoNumberst(self):
-                self.assertEqual(add("1,2"), 3)
-                
-        def test_ignoreNumbersGreaterThan1000(self):
-                self.assertEqual(add("1,1001"), 1)
-                
-        def test_expectSumWithCustomDelimiter(self):
-                self.assertEqual(add("//;\n1;2"), 3)
-                
-        def test_expectSumWithNewlineDelimiter(self):
-                self.assertEqual(add("1\n2,3"),6);
-        
 
-
-
+    def test_empty_string(self):
+        self.assertEqual(StringCalculator.add(""), 0)
+    
+    def test_single_number(self):
+        self.assertEqual(StringCalculator.add("1"), 1)
+    
+    def test_two_numbers(self):
+        self.assertEqual(StringCalculator.add("1,2"), 3)
+    
+    def test_multiple_numbers(self):
+        self.assertEqual(StringCalculator.add("1,2,3,4,5"), 15)
+    
+    def test_newline_delimiter(self):
+        self.assertEqual(StringCalculator.add("1\n2,3"), 6)
+    
+    def test_custom_delimiter(self):
+        self.assertEqual(StringCalculator.add("//;\n1;2"), 3)
+    
+    def test_negative_numbers(self):
+        with self.assertRaises(Exception) as context:
+            StringCalculator.add("1,-2,3")
+        self.assertEqual(str(context.exception), "Negatives not allowed: -2")
+    
+    def test_ignore_large_numbers(self):
+        self.assertEqual(StringCalculator.add("2,1001"), 2)
+    
+    def test_any_length_delimiter(self):
+        self.assertEqual(StringCalculator.add("//[***]\n1***2***3"), 6)
 
 if __name__ == '__main__':
     unittest.main()
-
-
